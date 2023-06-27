6262a0c604d0a9366fb6423284dcdb4f495d9dbd
Shalitha Lakruwan <69634572+shalithacode@users.noreply.github.com>
2023-06-27 14:05:39 +0530

    Replace Employee APIs with Spring data Rest

--- src/main/java/com/crud/employee_api/DAO/EmployeeRepository.java
+++ src/main/java/com/crud/employee_api/DAO/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.crud.employee_api.DAO;
 
 import com.crud.employee_api.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
